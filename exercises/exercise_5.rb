d2efea57e7d0bb6c5b03c95f59e277b577d4cc82
sdsog <34170634+sdsog@users.noreply.github.com>
2019-07-09 20:44:09 +0000

    completes exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -1,10 +1,21 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+puts 'Exercise 5'
+puts '----------'
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+
+puts @total_revenue.to_s
+
+puts ''
+puts '------- ANNUAL REVENUE -------'
+puts @total_revenue
+puts ''
