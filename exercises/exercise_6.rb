b15df34dae47bcde44e830879dcbbdb3dcba896a
sdsog <34170634+sdsog@users.noreply.github.com>
2019-07-09 22:17:19 +0000

    adds exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -5,7 +7,65 @@ require_relative './exercise_3'
 require_relative './exercise_4'
 require_relative './exercise_5'
 
-puts "Exercise 6"
-puts "----------"
+puts 'Exercise 6'
+puts '----------'
 
 # Your code goes here ...
+
+@store1.employees.create do |c|
+  c.first_name = 'Khurram'
+  c.last_name = 'Virani'
+  c.hourly_rate = 60
+end
+
+@store1.employees.create do |c|
+  c.first_name = 'Sarah'
+  c.last_name = 'Bonkers'
+  c.hourly_rate = 12
+end
+
+@store1.employees.create do |c|
+  c.first_name = 'Jerry'
+  c.last_name = 'Peters'
+  c.hourly_rate = 45
+end
+
+@store1.employees.create do |c|
+  c.first_name = 'Rob'
+  c.last_name = 'Carson'
+  c.hourly_rate = 80
+end
+
+@store2.employees.create do |c|
+  c.first_name = 'Flippy'
+  c.last_name = 'Joeman'
+  c.hourly_rate = 60
+end
+
+@store2.employees.create do |c|
+  c.first_name = 'Kerry'
+  c.last_name = 'Rupers'
+  c.hourly_rate = 72
+end
+
+@store2.employees.create do |c|
+  c.first_name = 'Singar'
+  c.last_name = 'Kepop'
+  c.hourly_rate = 15
+end
+
+@store2.employees.create do |c|
+  c.first_name = 'Zonk'
+  c.last_name = 'Nerphy'
+  c.hourly_rate = 100
+end
+
+
+# @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+
+# @store1.employees.create(first_name: "Lijing", last_name: "Wang", hourly_rate: 70)
+
+# @store1.employees.create(first_name: "Cheng", last_name: "Shi", hourly_rate: 80)
+
+# @store2.employees.create(first_name: "Hong", last_name: "Li", hourly_rate: 80)
+
--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
@@ -6,7 +8,14 @@ require_relative './exercise_4'
 require_relative './exercise_5'
 require_relative './exercise_6'
 
-puts "Exercise 7"
-puts "----------"
+puts 'Exercise 7'
+puts '----------'
 
 # Your code goes here ...
+
+puts 'ENTER STORE NAME: '
+new_store = $stdin.gets.chomp
+
+new_store = Store.new(name: 'new_store', annual_revenue: 100_000, mens_apparel: false, womens_apparel: false)
+new_store.valid?
+puts new_store.errors.messages
