95571ec9366316859305f10007aec1310f041a10
sdsog <34170634+sdsog@users.noreply.github.com>
2019-07-09 19:53:16 +0000

    completes exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,6 +1,31 @@
+# frozen_string_literal: true
+
 require_relative '../setup'
 
-puts "Exercise 1"
-puts "----------"
+puts 'Exercise 1'
+puts '----------'
 
 # Your code goes below here ...
+
+burnaby = Store.create do |c|
+  c.name = 'Burnaby'
+  c.annual_revenue = 300_000
+  c.mens_apparel = true
+  c.womens_apparel = true
+end
+
+richmond = Store.create do |c|
+  c.name = 'Richmond'
+  c.annual_revenue = 1_260_000
+  c.mens_apparel = false
+  c.womens_apparel = true
+end
+
+gastown = Store.create do |c|
+  c.name = 'Gastown'
+  c.annual_revenue = 190_000
+  c.mens_apparel = true
+  c.womens_apparel = false
+end
+
+puts Store.count
